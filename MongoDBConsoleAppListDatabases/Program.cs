44da1e2b448dcaa40ba3b7d6f636e2b17655129d
Aydın Türker <doktoralban@outlook.com>
2020-10-09 14:45:35 +0300

    Add project files.

---
+++ MongoDBConsoleAppListDatabases/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace MongoDBConsoleAppListDatabases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases are:");
+
+            foreach (var item in dbList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
